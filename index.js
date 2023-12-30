c695d097d0decc0c6d589c6feaff1622cd02bc5c
unknown <himanshukanwar2001@gmail.com>
2023-12-30 07:22:52 +0530

    added the express ejs layout plugins and created new ejs files

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+const expressLayouts = require("express-ejs-layouts");
 
 const path = require("path");
+
+app.use(expressLayouts);
 
 //use express router
 app.use("/", require("./routes"));
 
-
 //setup the view engine
 app.set("view engine", "ejs");
-app.set("views",path.join(__dirname,"views"));
-
-
+app.set("views", path.join(__dirname, "views"));
 
 app.listen(port, function (err) {
   if (err) {
