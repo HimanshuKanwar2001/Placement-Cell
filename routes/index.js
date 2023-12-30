9847ed183913e3978561bb0d1ddea8677a8b6e78
unknown <himanshukanwar2001@gmail.com>
2023-12-30 07:15:38 +0530

    added user controller ,header,footer using partials method and ejs files

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,10 @@ const homeController=require('../controllers/home_controller');
 
 router.get("/", homeController.home);
 
-console.log("Router loaded");
+//users routes here
+router.use('/users',require('./users'));
+
+
 
 
 module.exports=router;
