c781957a9f9c9e914e78b3a1b55920a217706d43
unknown <himanshukanwar2001@gmail.com>
2024-01-01 05:57:01 +0530

    completed manual authentication and made signout link

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,9 +1,24 @@
 const User = require("../models/user");
 
-module.exports.profile = function (req, res) {
-  return res.render("user_profile", {
-    title: "Profile Page",
-  });
+module.exports.profile = async function (req, res) {
+  try {
+    if (req.cookies.user_id) {
+      const user = await User.findById(req.cookies.user_id);
+      console.log("user", user);
+      if (user) {
+        return res.render("user_profile", {
+          title: "User Profile",
+          user: user,
+        });
+      }
+      return res.redirect("/users/sign-in");
+    } else {
+      return res.redirect("/users/sign-in");
+    }
+  } catch (err) {
+    console.error("Error:", err);
+    return res.status(500).send("Internal Server Error");
+  }
 };
 
 //render Sign Up page
@@ -43,6 +58,37 @@ module.exports.create = async function (req, res) {
 };
 
 //sing in and create a session for the user
-module.exports.createSession = function (req, res) {
-  //TODO later
+module.exports.createSession = async function (req, res) {
+  try {
+    //steps to authenticate
+    //find the user
+    const user = await User.findOne({ email: req.body.email });
+    //handle user found
+    if (user) {
+      //handle password which doesn't match
+
+      if (user.password != req.body.password) {
+        return res.redirect("back");
+      }
+
+      //handle session creation
+      res.cookie("user_id", user.id);
+      return res.redirect("/users/profile");
+    } else {
+      // handle user not found
+      return res.redirect("back");
+    }
+  } catch (err) {
+    console.error("Error:", err);
+    return res.status(500).send("Internal Server Error");
+  }
+};
+//sign out from the profile
+module.exports.signOut = function (req, res) {
+  if (req.cookies.user_id) {
+    res.clearCookie("user_id");
+    return res.redirect("/users/sign-in");
+  } else {
+    return res.redirect("/users/sign-in");
+  }
 };
