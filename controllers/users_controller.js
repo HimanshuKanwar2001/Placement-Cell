e4ac3f6f40bbf6b6be2e9f332fdde25dd13acc9c
unknown <himanshukanwar2001@gmail.com>
2024-01-01 18:27:04 +0530

    setting passport with session and passing user data to views and limiting page access

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -8,6 +8,10 @@ module.exports.profile = function (req, res) {
 
 //render Sign Up page
 module.exports.signUp = function (req, res) {
+
+  if (req.isAuthenticated()) {
+    return res.redirect("/users/profile");
+  }
   return res.render("user_sign_up", {
     title: "Placement Cell | Sign Up",
   });
@@ -15,6 +19,10 @@ module.exports.signUp = function (req, res) {
 
 //render Sign In page
 module.exports.signIn = function (req, res) {
+
+  if (req.isAuthenticated()) {
+    return res.redirect("/users/profile");
+  }
   return res.render("user_sign_in", {
     title: "Placement Cell | Sign In",
   });
@@ -44,5 +52,5 @@ module.exports.create = async function (req, res) {
 
 //sing in and create a session for the user
 module.exports.createSession = function (req, res) {
-  //TODO later
+  return res.redirect("/");
 };
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
-const port = 8080;
+const port = 8000;
 const expressLayouts = require("express-ejs-layouts");
 const path = require("path");
 const db = require("./config/mongoose");
+//used for session cookie
+const session = require("express-session");
+const passport = require("passport");
+const passportLocal = require("./config/passport-local-strategy");
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -16,12 +20,34 @@ app.use(expressLayouts);
 //extract styles and scripts from sub pages into the layout
 app.set("layout extractStyles", true);
 app.set("layout extractScripts", true);
-//use express router
-app.use("/", require("./routes"));
+
+
 
 //setup the view engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+app.use(
+  session({
+    name: "placement_cell",
+    //TODO change the secret before deployment in production mode
+    secret: "blahsomething",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 1000 * 60 * 100,
+    },
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
+
+//use express router
+app.use("/", require("./routes"));
+
 
 app.listen(port, function (err) {
   if (err) {
